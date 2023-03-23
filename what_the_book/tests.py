18b9909439ab6f07ed24dc8a88ced50978e61e78
Miko <2628565o@student.gla.ac.uk>
2023-03-23 16:46:01 +0000

    fixed more date issues!

--- what_the_book/tests.py
+++ what_the_book/tests.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from django.test import TestCase
 from django.shortcuts import reverse
 from what_the_book.models import *
@@ -6,22 +6,23 @@ from what_the_book.models import *
 
 class ModelTests(TestCase):
     def test_likes_non_negative(self):
-        review = Review(title="Title", createdOn=datetime.now(), likes=-1)
-        review.save()   
+        review = Review(
+            title="Title", createdOn=datetime.now().date(), likes=-1)
+        review.save()
 
         self.assertEqual((review.likes >= 0), True)
 
     def test_valid_date(self):
         review = Review(
-            title="Title", createdOn=datetime.now() + timedelta(days=1))
+            title="Title", createdOn=datetime.now().date() + timedelta(days=1))
         review.save()
-
-        self.assertEqual((review.createdOn <= datetime.now()), True)
+        self.assertEqual((review.createdOn <= datetime.now().date()), True)
 
     def test_valid_book_slug(self):
         book = Book(title="A BOOK ABOUT SHOUTING")
         book.save()
         self.assertEqual((book.slug == "a-book-about-shouting"), True)
+
 
 class ViewTests(TestCase):
     def test_home_with_no_books(self):
